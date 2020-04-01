f085317ed28e2cf3989bff7e2e874e8edf16451a
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-01 16:07:30 -0300

    Exercicios de interpretação de código

--- semana3/loops/js-vanilla-template/index.js
+++ semana3/loops/js-vanilla-template/index.js
@@ -1,100 +1,57 @@
-/* 1--->
-resultado = a. false
+//**Exercícios de interpretação de código**
 
-resultado = b. false
+// EXERCÍCIO 1
 
-resultado = c. true
+// O que o código abaixo está fazendo? Qual o resultado impresso no console? 
 
-resultado = d. false
+// let sum = 0
+    
+//     for(let i = 0; i < 15; i++) {
+//       sum += i 
+//     }
+//     console.log(sum) 
+//Passo 1:   i = 0 ; i < 15; i = 0 + 1, logo i agora tem o valor de 1
+//           sum = 0 + 1 
+//Passo 2:   i = 1; i = 1 + 1, i agora tem o valor de 2
+//           sum = 1 + 2, sum agora tem o valor de 3
+//e assim sucessivamente, até que i seja menor que 15.
+//O RESULTADO IMPRESSO NO CONSOLE É: 105
 
-resultado = e. boolean 
+// EXERCÍCIO 2
 
-2--->
-a. São maneiras de guardar mais de uma informação ao mesmo tempo.
-b. [0]
-c. array.length
-d.  I- undefined
-    II- null
-    III- 11
-    IV- 3 e 4
-    V- 19 e 9
-    VI- 3
-    VII- 1
-*/
+// Leia o código abaixo:
 
-// 1. Convertendo
-//A. 
-const FAHRENHEIT1 = 77;
-const KELVIN1 = (FAHRENHEIT1 - 32)*5/9 + 273.15;
-console.log("a. KELVIN - ", KELVIN1);
-//B.
-const CELSIUS = 80;
-const FAHRENHEIT2 = CELSIUS*9/5 + 32;
-console.log("b. FAHRENHEIT - ", FAHRENHEIT2);
-//C.
-const KELVIN3 = (30 - 32)*5/9 + 273.15;
-const FAHRENHEIT3 = 30*9/5 + 32;
-console.log("c. FAHRENHEIT -", FAHRENHEIT3, 
-"- KELVIN- ", KELVIN3);
-//D.
-let VALOR = prompt("Qual valor você deseja converter para Kelvin e Fahrenheit?");
-let KELVIN4 = (VALOR- 32)*5/9 + 273.15;
-let FAHRENHEIT4 = VALOR*9/5 + 32;
-console.log("d. FAHRENHEIT - ", FAHRENHEIT4, "KELVIN - ", KELVIN4);
+// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+// const novaLista = []
+// const numero = 5
+//     for(const item of lista){
+//       if(item%numero === 0) {
+//         novaLista.push(item)
+//       }
+//     }
+//     console.log(novaLista)
 
-// 2. Questionário
-const nome = prompt("1. Qual o seu nome?")
-console.log("1. Qual o seu nome? "+"\n Resposta: "+nome)
+// a. O que o comando `.push` faz? o comando `.push` adiciona um novo indíce na array
 
-const idade = prompt("2. Qual a sua idade?")
-console.log("2. Qual a sua idade?"+" Resposta\n "+idade+" anos.")
+// b. Qual o valor impresso no console? Os números divisivel por 5.
+// (4) [10, 15, 25, 30]
+// 0: 10
+// 1: 15
+// 2: 25
+// 3: 30
+// length: 4
+// __proto__: Array(0)
 
-const cor = prompt("3. Qual a sua cor favorita?")
-console.log("3. Qual a sua cor favorita? "+"\n Resposta: "+cor)
+// c.1. Qual seria imprimido no console se a variável `numero` tivesse o valor de `3`? 
+// Os valores que são divisiveis por 03 (12, 15, 18, 21, 27, 30). 
+// c.2.E se tivesse o valor de `4`?
 
-const ifood = prompt("4. O que você pediria no Ifood agora?")
-console.log("4. O que você pediria no Ifood agora?"+"\n Resposta: "+ifood)
-
-const pedidoIfood = prompt("5. Você costuma sempre pedir isso no Ifood? :D")
-console.log("5. Você costuma sempre pedir isso no Ifood?"+"\n Resposta: "+pedidoIfood)
-
-//3. Consumo de kWh
-let consumo = prompt("Qual foi seu consumo de quilowatt-hora (kWh) da sua resicência?")
-valorConsumo = consumo*0.05;
-console.log("O valor do seu consumo é R$"+valorConsumo)
-
-let consumoCliente = 280*0.05;
-console.log("O valor de consumo é de R$ "+consumoCliente)
-
-let desconto = consumoCliente*15/100
-console.log("O valor a ser pago com desconto é R$ "+desconto)
-
-// DESAFIO
-//a.
-let valorLbs = 20/2.2;
-console.log("a. 20lb equivalem a "+valorLbs+" kg");
-
-//b.
-let valorOz = 10.5/35.274;
-console.log("b. 10.5oz equivalem a "+valorOz+" kg");
-
-//c.
-let valorMi = 100*1609;
-console.log("c. 100mi equivalem a "+valorMi+" m");
-
-//d.
-let valorFt = 50/3.281;
-console.log("d. 50ft equivalem a "+valorFt+" m");
-
-//e.
-let valorGal = 103.56*379;
-console.log("e. 103.56gal equivalem a "+valorGal+" l");
-
-//f. 
-let valorXic = 450/3.52;
-console.log("f. 450 xic equivalem a "+valorXic+" l");
-
-//g.
-const valor = prompt("Qual o valor em oz que você deseja converter para Kg?")
-let valorOz2 = valor/35.274;
-console.log("g. "+valor+" equivalem a "+valorOz2+" kg");
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+const novaLista = []
+const numero = 3
+for(const item of lista){
+  if(item%numero === 0) {
+    novaLista.push(item)
+  }
+}
+console.log(novaLista)
