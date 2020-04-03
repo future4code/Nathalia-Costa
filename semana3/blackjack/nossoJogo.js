a66bf31dc2a829e635b8667d8d69889505dd4e3c
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-03 16:15:33 -0300

    Cartas e pontuações

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -10,3 +10,19 @@
  * 
  * 
  */
+console.log("Bem vindo ao jogo de Blackjack!")
+
+if(confirm("Quer iniciar uma nova rodada?")) {
+   const carta1 = comprarCarta(); 
+   const carta2 = comprarCarta(); 
+   const carta3 = comprarCarta(); 
+   const carta4 = comprarCarta();
+   const valor1 = carta1.valor+carta2.valor;
+   const valor2 = carta3.valor+carta4.valor;
+
+console.log("Usuário - cartas: "+carta1.texto+" "+carta2.texto+" - Pontuação ", valor1,
+"\nComputador - cartas: "+carta3.texto+" "+carta4.texto+" - Pontuação ", valor2);
+}
+else {
+	console.log("O jogo acabou. :(")
+}
