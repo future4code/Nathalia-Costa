600776134cb99894de0aa8f549db0c771da3495d
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-06 18:48:54 -0300

    Resolução do exercicio 03

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -45,3 +45,51 @@
 // __proto__: Array(0)
 
 // Exercício 02
+// a. 0 / 2 / undefined
+
+// b.
+// Quando troquei os nomes por números, apareceu em 
+// qual ordem da array o elemento está.
+// Dessa forma...
+
+// let arrayDeNomes = [4, 5, 7, 6, 3];
+// 			     //[0, 1, 2, 3, 4]
+// const funcao = (lista, nome) => {
+//   for (let i = 0; i < lista.length; i++) {
+//     if (lista[i] === nome) {
+//       return i;
+//     }
+//   }
+// };
+
+// console.log(funcao(arrayDeNomes, 4)); //[0]
+// console.log(funcao(arrayDeNomes, 7)); //[2]
+// console.log(funcao(arrayDeNomes, 6)); //[3]
+
+// Exercício 03
+
+//Esse código decreve a array jenifer com dois elementos,
+//na função for ela soma e multiplica os valor estabelecidos
+//no escopo da function metodo e logo após ela entra na nova 
+//array chamada carlos e retorna.
+
+
+// jeniffer = [3, 1]
+// console.log(jeniffer)
+// function metodo(jeniffer) {
+// 	let resultadoA = 0;
+// 	let resultadoB = 1;
+// 	let carlos = [];
+  
+// 	for (let x of jeniffer) {
+// 	  resultadoA += x;
+// 	  resultadoB *= x;
+// 	}
+  
+// 	carlos.push(resultadoA);
+// 	carlos.push(resultadoB);
+// 	return carlos;
+	
+//   }
+//   const mostra = metodo(jeniffer)
+//   console.log(mostra)
