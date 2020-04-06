04881732feb666ea3650caca0cd647becd4ebda3
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-06 16:44:13 -0300

    Resolução do exercicio 02

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -1,15 +1,47 @@
 //**Exercícios de interpretação de código**
 // Exercício 01
-//
+//const minhaFuncao = (quantidade) => {
+// 	const array = []
+// 	for(let i = 0; i < quantidade; i+=2) {
+// 			for(let j = 0; j < i; j++) {
+// 				array.push(j)
+// 			}
+// 	}
+// 	return array
+// }
 
-const minhaFuncao = (quantidade) => {
-	const array = []
-	for(let i = 0; i < quantidade; i+=2) {
-			for(let j = 0; j < i; j++) {
-				array.push(j)
-			}
-	}
-	return array
-}
+// console.log(minhaFuncao(8))
 
-minhaFuncao()
+// a. []
+// length: 0
+// __proto__: Array(0)
+
+// b. 
+// (6) [0, 1, 0, 1, 2, 3]
+// 0: 0
+// 1: 1
+// 2: 0
+// 3: 1
+// 4: 2
+// 5: 3
+// length: 6
+// __proto__: Array(0)
+
+// c. 
+// (12) [0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5]
+// 0: 0
+// 1: 1
+// 2: 0
+// 3: 1
+// 4: 2
+// 5: 3
+// 6: 0
+// 7: 1
+// 8: 2
+// 9: 3
+// 10: 4
+// 11: 5
+// length: 12
+// __proto__: Array(0)
+
+// Exercício 02
