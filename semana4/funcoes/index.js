bdcfa4b04ecc7032436c28f5ca40c922eb532226
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-06 15:35:19 -0300

    Iniciando o exercicio 1

--- semana4/funcoes/index.js
+++ semana4/funcoes/index.js
@@ -1,3 +1,15 @@
 //**Exercícios de interpretação de código**
 // Exercício 01
 //
+
+const minhaFuncao = (quantidade) => {
+	const array = []
+	for(let i = 0; i < quantidade; i+=2) {
+			for(let j = 0; j < i; j++) {
+				array.push(j)
+			}
+	}
+	return array
+}
+
+minhaFuncao()
