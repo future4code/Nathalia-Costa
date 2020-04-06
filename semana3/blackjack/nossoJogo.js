a206f923baf15da164fdcac9d9cafc0d488c4ad8
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-06 13:44:19 -0300

    Adicionando as condições

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -2,7 +2,7 @@
  * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
  * 
  * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    let carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
     
     console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
     console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
@@ -12,17 +12,31 @@
  */
 console.log("Bem vindo ao jogo de Blackjack!")
 
-if(confirm("Quer iniciar uma nova rodada?")) {
-   const carta1 = comprarCarta(); 
-   const carta2 = comprarCarta(); 
-   const carta3 = comprarCarta(); 
-   const carta4 = comprarCarta();
-   const valor1 = carta1.valor+carta2.valor;
-   const valor2 = carta3.valor+carta4.valor;
+let valorUsuario = 0;
+let valorPc = 0;
+let cartasUsuarios = "Usuário - cartas: ";
+let cartasPc = " Computador - cartas: ";
 
-console.log("Usuário - cartas: "+carta1.texto+" "+carta2.texto+" - Pontuação ", valor1,
-"\nComputador - cartas: "+carta3.texto+" "+carta4.texto+" - Pontuação ", valor2);
-}
-else {
-	console.log("O jogo acabou. :(")
+if(confirm("Quer iniciar uma nova rodada?")) {
+   for(let i = 0; i < 2; i++){
+      let carta = comprarCarta();
+      cartasUsuarios = cartasUsuarios + carta.texto;
+      valorUsuario = valorUsuario + carta.valor;
+
+      let carta2 = comprarCarta();
+      cartasPc = cartasPc + carta2.texto;
+      valorPc = valorPc + carta2.valor;
+   }
+   console.log(cartasUsuarios+" - Pontuação ", valorUsuario,cartasPc+" - Pontuação ", valorPc);
+   if(valorUsuario > valorPc){
+      console.log("O usuario ganhou")
+   }
+   else if(valorPc > valorUsuario){
+      console.log("O computador ganhou")
+   }
+   else{
+      console.log("Empate")
+   }
+}else {
+   console.log("O jogo acabou.")
 }
