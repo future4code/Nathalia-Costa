f48ca3c3b539535c8ce0e2b133bfa66f3a48edb3
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-07 19:59:54 -0300

    Exercicio completo sem os desafios

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,10 +1,37 @@
 function adicionarTarefa(){
     const nomeDaTarefa = document.getElementById("novaTarefa").value;
-    document.getElementById("espacotexto").innerHTML += "<p>"+nomeDaTarefa+"</p>"
+    const diasDaSemana = document.getElementById("diasDaSemana").value;
     
+    switch(diasDaSemana){
+        case 'SEG' :
+            document.getElementById("tarefaSegunda").innerHTML += "<li>"+nomeDaTarefa+"</li>";
+            break;
+        case 'TER' :
+            document.getElementById("tarefaTerca").innerHTML += "<li>"+nomeDaTarefa+"</li>";
+            break;
+        case 'QUA' :
+            document.getElementById("tarefaQuarta").innerHTML += "<li>"+nomeDaTarefa+"</li>";
+            break;
+        case 'QUI' :
+            document.getElementById("tarefaQuinta").innerHTML += "<li>"+nomeDaTarefa+"</li>";
+            break;
+        case 'SEX' :
+            document.getElementById("tarefaSexta").innerHTML += "<li>"+nomeDaTarefa+"</li>";
+            break;
+        case 'SAB' :
+            document.getElementById("tarefaSabado").innerHTML += "<li>"+nomeDaTarefa+"</li>";
+            break;
+        case 'DOM' :
+            document.getElementById("tarefaDomingo").innerHTML += "<li>"+nomeDaTarefa+"</li>";
+            break;
+    }
+    document.getElementById("novaTarefa").value = "";
 }
-adicionarTarefa()
 
+//limpar tarefas
+// function limpar(){
+//     document.getElementsById("tarefaSegunda").value = "";
+// }
 
 //como riscar tarefa
 // function meRisca(coisaID){
