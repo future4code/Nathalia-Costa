5a952f93d5e3b5d8247702a3b58c62819ec7386d
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-08 20:13:24 -0300

    exercicio finalizado

--- semana4/objetos/blog/index.js
+++ semana4/objetos/blog/index.js
@@ -2,18 +2,22 @@ function adicionarPost(){
     const titulo = document.getElementById("titulo").value;
     const autor = document.getElementById("autor").value;
     const texto = document.getElementById("texto").value;
+    const imagem = document.getElementById("inserirImagem").value;
+    const postagem = document.getElementById("post").innerHTML;
 
-    document.getElementById("post").innerHTML += "<article><ul>"+titulo+" - "+autor+"</ul>"+'<li>'+texto+'</li></article>';
+    //inserindo os value em uma array
+    const publicado = 
+    [
+        {titulo: titulo, autor: autor, texto: texto, imagem: imagem},
+    ]
+
+    //limpando os campos do formulario
     document.getElementById('titulo').value = "";
     document.getElementById('autor').value = "";
     document.getElementById('texto').value = "";
+    document.getElementById('inserirImagem').value = "";
 
-    const publicado = {
-        titulo: titulo,
-        autor: autor,
-        texto: texto,
-        foiPublicado: true
-    }
-    console.log(publicado)
+    //inserindo value no HTML
+    document.getElementById("post").innerHTML = '<article><ul>'+titulo+'</ul>'+'<ol>'+texto+'</ol>'+'<ol><img src='+imagem+'></ol><ol> - '+autor+'<ol></article>'+postagem;
+
 }
-
