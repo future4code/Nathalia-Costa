b3118c53a8b5c29b4b736b15b453d6a04a33e7bd
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-11 18:23:31 -0300

    Exercicio 2.investeDinheiro

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -6,3 +6,9 @@
 // cotação enviada no Prompt. Mostrada no conlose pela variavel meuDinheiro que 
 // apresenta o valor final da multiplicação.
 
+/* 2. function investeDinheiro tem duas variaveis uma para saber qual tipo de investimento
+e outra o valor a ser investido. Com um Switch ele analisa qual foi o tipo de investimento 
+e assim aplicar o valor em juros. O return volta com o valor investido de acordo com o tipo
+informado. Após a function ele dar duas variaveis de investimento. Sendo o primeiro em Ações 
+que terá um retorno no console. E o segundo em Tesouro Direto, porém não existe esse tipo de 
+financiamento e será enviado um alert informando "TIPO DE INVESTIMENTO INFORMADO INCORRETO".*/
