ff07006003e0b9289669889786d8e74bb99a2c34
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-11 18:07:47 -0300

    Exercicio 1.conversorDeMoeda

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,8 @@
+// Exercício de Leitura de código
+// Analisar, escrever o que ele faz, como faz e qual será o valor impresso no console.
+
+// 1. Ele converte o valor em Dolar para Reais. Ele solicita qual o valor da cotação 
+// do dolar através de um Prompt e dar um return com o valor indicado na função * a 
+// cotação enviada no Prompt. Mostrada no conlose pela variavel meuDinheiro que 
+// apresenta o valor final da multiplicação.
+
