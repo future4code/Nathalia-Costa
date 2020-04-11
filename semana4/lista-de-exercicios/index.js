1d8960730e59a57767a32fcbeb686a07e4315a4f
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-11 18:33:32 -0300

    Exercicio 3.arrays de pares e impares

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -12,3 +12,10 @@ e assim aplicar o valor em juros. O return volta com o valor investido de acordo
 informado. Após a function ele dar duas variaveis de investimento. Sendo o primeiro em Ações 
 que terá um retorno no console. E o segundo em Tesouro Direto, porém não existe esse tipo de 
 financiamento e será enviado um alert informando "TIPO DE INVESTIMENTO INFORMADO INCORRETO".*/
+
+/* 3. Existe 3 arrays [numeros, array1, arrays2], a array numeros[] está preenchidas com alguns,
+numeros e logo abaixo há um for...of para que os numeros dessa arrays sejam divididos em pares e
+ímpares. Se par inseridas no array1, caso não insirir no array2. 
+Com isso o console.log("Quantidade (...), numeros.length") imprimirá a quantidade total de número 
+na array numeros. Os demais consoles exibirão a quantidade de numeros da array1 com pares e da array2 
+com os impares.
