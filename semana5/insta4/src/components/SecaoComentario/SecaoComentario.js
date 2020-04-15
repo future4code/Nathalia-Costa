e70890033366a17ef24187f71fee239139bbf36c
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-15 16:10:49 -0300

    Exercicio finalizado

---
+++ semana5/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import './SecaoComentario.css'
+
+export class SecaoComentario extends Component {
+	state = {
+		valorComentario: ""
+	};
+
+	onChangeComentario = event => {
+		this.setState({ valorComentario: event.target.value });
+		console.log(this.state.valorComentario)
+	};
+
+	render() {
+		return <div className={'comment-container'}>
+			<input
+				className={'input-comentario'}
+				placeholder={'Comentário'}
+				value={this.state.valorComentario}
+				onChange={this.onChangeComentario}
+			/>
+			<button onClick={this.props.aoEnviar}>Enviar</button>
+		</div>
+	}
+}
