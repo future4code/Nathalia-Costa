a6618dac54449c210d27539d5c725e06cb9a59bc
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-22 21:05:15 -0300

    pronto

---
+++ semana6/aula22/boiler-plate/build/precache-manifest.946f22db0b0283ee0ea7c60a9db36fda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f71bf2047b4e61d230e25fe8f8cd079",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43a0afe599e9bf1c072a",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "078d8750ac3017505296",
+    "url": "/static/js/2.29e9a946.chunk.js"
+  },
+  {
+    "revision": "43a0afe599e9bf1c072a",
+    "url": "/static/js/main.28b53d69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
