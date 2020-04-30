70ebbef642115c7ee5985ffb793a14dea0c6b8de
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-04-30 16:46:22 -0300

    Arquivos basicos

---
+++ semana7/aula25/labenuser/build/precache-manifest.ac5998e53a5fd477d7966d1897e4ba1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df49cb8c5a1496b2ad10ab84e3b1bec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e402eedb15c9bdee1ea3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "933b475dd7de697c8a95",
+    "url": "/static/js/2.2408c751.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2408c751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e402eedb15c9bdee1ea3",
+    "url": "/static/js/main.09a64f16.chunk.js"
+  },
+  {
+    "revision": "5363d4502c1ebcd17d47",
+    "url": "/static/js/runtime-main.208a5ad4.js"
+  }
+]);
