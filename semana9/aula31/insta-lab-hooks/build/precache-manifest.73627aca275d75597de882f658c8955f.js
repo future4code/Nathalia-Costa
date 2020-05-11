8ce7fc9c2266c51b373a5d91f6e0fa83ed54edd8
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-05-11 20:02:01 -0300

    Build

---
+++ semana9/aula31/insta-lab-hooks/build/precache-manifest.73627aca275d75597de882f658c8955f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b8c6f2e4e67d7230c3909be1259e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fc89151608cefd6b4e1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "66813cf8f116e464260c",
+    "url": "/static/js/2.c579f815.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.c579f815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc89151608cefd6b4e1",
+    "url": "/static/js/main.1676831c.chunk.js"
+  },
+  {
+    "revision": "fbdb0ec750bc986cd3d5",
+    "url": "/static/js/runtime-main.40261604.js"
+  },
+  {
+    "revision": "de299b47c15bad3a33ad36319a10bef3",
+    "url": "/static/media/comment_icon.de299b47.svg"
+  },
+  {
+    "revision": "042aa417f110588538aefef728ed90fa",
+    "url": "/static/media/favorite-white.042aa417.svg"
+  },
+  {
+    "revision": "4c67d8751e56b25704ac9b791835bef8",
+    "url": "/static/media/favorite.4c67d875.svg"
+  }
+]);
