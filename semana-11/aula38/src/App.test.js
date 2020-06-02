9eb17c03e499129886faed51b6b203a960fad5dc
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-06-02 17:01:55 -0300

    iniciando os testes pt2

--- semana-11/aula38/src/App.test.js
+++ semana-11/aula38/src/App.test.js
@@ -19,29 +19,41 @@ describe('Renderização inicial', () => {
 // 1. O usuário deve poder criar um novo post. Para isso, ele deve digitar no input cujo placeholder é "Novo post", 
 // e clicar no botão "Adicionar". Quando ele faz isso, um novo item é adicionado à lista de posts.
 
-// describe('', () => {
-//     Test('', )
-// })
+describe('', () => {
+    test('Criar um novo post', () => {
+        const {getByPlaceholderText} = render(<App/>)
 
+        const input = getByPlaceholderText('Novo post')
 
+        fireEvent.change(input, {
+            target: {
+                value: 'tarefa teste'
+            }
+        })
 
+        const button = getByText(/Adicionar/)
 
-
-
-
+        fireEvent.click(button)
+        expect(input).toHaveValue('tarefa teste')
+    })
+})
 
 
 
 // 2. O usuário deve poder curtir um post. Isso é feito clicando no botão "Curtir" em um post. 
 // Quando isso é feito, o botão que foi clicado deve trocar seu texto para "Descurtir".
 
-
-
-
-
-
-
-
+describe('', () => {
+    test('', () => {
+        const {getByPlaceholderText} = render(<App/>)
+    })
+})
 
 // 3. O usuário deve poder apagar um post. Isso é feito clicando no botão "Apagar" em um post. 
 // Quando isso é feito, o respectivo post deve sumir da tela.
+
+describe('', () => {
+    test('', () => {
+        const {getByPlaceholderText} = render(<App/>)
+    })
+})
