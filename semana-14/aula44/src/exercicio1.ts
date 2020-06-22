4a86fa0d223d85acbefae3b5250754c69c328f28
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-06-22 15:23:39 -0300

    exercicio 01

---
+++ semana-14/aula44/src/exercicio1.ts
@@ -0,0 +1,22 @@
+// a) Crie uma variável **minhaString** do tipo `string` e atribua um valor a ela. 
+// Tente atribuir um número a esta variável. O que acontece?
+
+// b) Crie uma variável **meuNumero** do tipo `number` e atribua um valor numérico. 
+// Como podemos fazer com que essa variável aceite letras?
+
+// c) Agora crie um novo objeto. Este objeto é uma pessoa, e deve possuir três propriedades:
+
+// `nome`, que é uma string;
+
+// `idade`, que é um número;
+
+// `corFavorita`, que é uma string.
+
+// Como você faria para garantir que o objeto só tenha as propriedades necessárias? 
+// Faça a tipagem do objeto para que ele só aceite os valores acima.
+
+// d) Crie mais três objetos, que também precisam ter apenas os campos definidos acima. 
+// Crie um **tipo** `Pessoa` para garantir que todos os objetos tenham os mesmos campos.
+
+// e) Modifique o tipo de objeto para que possamos apenas escolher entre as cores do arco-íris. 
+// Use um `enum` para isso.
