b8312cb5928f452d1280b0229d034d179846cb6b
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-06-22 18:03:40 -0300

    exercicio 03

---
+++ semana-14/aula44/src/exercicio3.ts
@@ -0,0 +1,5 @@
+// Faça uma função que receba um array de números e 
+// retorne um objeto que tenha as seguintes informações: 
+// a quantidade de números que há no array, 
+// a quantidade de números ímpares no array e a 
+// soma de todos os elementos do array.
