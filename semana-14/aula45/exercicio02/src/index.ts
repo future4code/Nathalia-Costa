7cc79dea56df20318f2869ce26800fee5f2c054e
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-06-23 18:58:20 -0300

    colors ex 02

--- semana-14/aula45/exercicio02/src/index.ts
+++ semana-14/aula45/exercicio02/src/index.ts
@@ -3,20 +3,21 @@
 const operacao: string = process.argv[2];
 const numberOne = Number(process.argv[3]);
 const numberTwo = Number(process.argv[4]);
+const colors = require('colors')
 
 function aplicacao(operacao: string, numberOne: number, numberTwo: number){
     switch(operacao){
         case "add":
-            console.log(numberOne + numberTwo)
+            console.log(colors.green(numberOne + numberTwo))
         break;
         case "sub":
-            console.log(numberOne - numberTwo)
+            console.log(colors.red(numberOne - numberTwo))
         break;
         case "mult":
-            console.log(numberOne * numberTwo)
+            console.log(colors.magenta(numberOne * numberTwo))
         break;
         case "div":
-            console.log(numberOne / numberTwo)
+            console.log(colors.cyan(numberOne / numberTwo))
         break;
         default:
             break
