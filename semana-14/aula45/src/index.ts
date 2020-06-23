0455d0a3f008fd4132d2f0327e84f4fc1d3650ee
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-06-23 17:46:23 -0300

    exercicio 03 concluido, usando writeFileSync

--- semana-14/aula45/src/index.ts
+++ semana-14/aula45/src/index.ts
@@ -37,3 +37,19 @@ import * as fs from 'fs';
 // aplicacao(operacao, numberOne, numberTwo)
 
 // EXERCICIO 03
+function adicionarTarefa(caminho: string, tarefa:string): void{
+    const bufferArquivo: Buffer = fs.readFileSync(caminho);
+    const texto: string = bufferArquivo.toString();
+    const tarefas: string[] = JSON.parse(texto);
+
+    tarefas.push(tarefa)
+    
+    const tarefasRefresh: string = JSON.stringify(tarefas)
+    fs.writeFileSync(caminho, tarefasRefresh)
+
+    console.log("Tarefa adicionada com sucesso!")
+}
+const caminho: string = process.argv[2];
+const tarefa: string = process.argv[3];
+
+adicionarTarefa(caminho, tarefa)
