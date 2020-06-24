1e92c88a3f7abf34582623ba3575b099064d104e
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-06-24 16:57:02 -0300

    inciando o exercicio

---
+++ semana-14/aula46/src/index.ts
@@ -0,0 +1,25 @@
+import * as moment from "moment"
+moment.locale("pt-br");
+
+type evento = {
+    nome: string,
+    descricao: string,
+    inicio: moment.Moment,
+    termino: moment.Moment
+};
+
+const TodosOsEventos: evento[] = [
+    {
+        nome: "São João",
+        descricao: "Festa junina",
+        inicio: moment("23/06/2020 18:00", "DD/MM/YYYY HH:mm"),
+        termino: moment("24/06/2020 02:00", "DD/MM/YYYY HH:mm")
+    },
+    {
+        nome: "Natal",
+        descricao: "Festa natalina",
+        inicio: moment("24/12/2020 20:00", "DD/MM/YYYY HH:mm"),
+        termino: moment("25/12/2020 23:00", "DD/MM/YYYY HH:mm")
+    }
+]
+console.log(TodosOsEventos)
