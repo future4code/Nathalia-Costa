5d03103dbb2375877bc5484725d01ad2bf66fcff
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-06-26 11:40:40 -0300

    Iniciando o projeto

---
+++ semana-14/projeto/src/index.ts
@@ -0,0 +1,11 @@
+// 1. Criem um tipo para representar uma conta para o usuário
+// 2. Criem mais um tipo: para representar as transações que 
+// serão salvas no extrato
+// 3. Criem uma função (`createAccount`) que será responsável por 
+// cadastrar um usuário em um arquivo `JSON`. Neste momento, não 
+// se preocupe, com as validações descritas nas funcionalidades.
+// 4. Criem uma função (`getAllAccounts`) que será responsável por 
+// pegar todos os usuários existentes no arquivo `JSON`.
+// 5. Adicione, uma validação na função do item 1 (`createAccount`): 
+// o usuário deve possuir mais do que 18 anos para conseguir se cadastrar. 
+// Caso não possua, exibam uma mensagem de erro.
