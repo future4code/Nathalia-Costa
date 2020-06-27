1ae23edea12d8faced8a93e9b4640b51d8944b66
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-06-27 00:26:03 -0300

    atualizando e fazendo as verificações

--- semana-14/projeto/build/index.js
+++ semana-14/projeto/build/index.js
@@ -3,13 +3,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const moment_1 = __importDefault(require("moment"));
 const fs_1 = __importDefault(require("fs"));
-function criarConta(nome, cpf, nascimento) {
+function criarConta(nome, nascimento, cpf) {
+    const dataNasc = moment_1.default(nascimento, "DD/MM/YYYY");
+    const hoje = moment_1.default();
+    const idade = hoje.diff(dataNasc, "years");
+    if (idade < 18) {
+        console.log("Necessário que o usuário tenha mais de 18 anos");
+        return;
+    }
+    ;
     const usuariosFilePath = "C:/Users/Larissa/Downloads/Repositório - GitHub/Nathalia-Costa/semana-14/projeto/src/usuarios.json";
     const usuariosFileData = fs_1.default.readFileSync(usuariosFilePath);
     const usuariosString = usuariosFileData.toString();
-    console.log(usuariosString);
     const usuarios = JSON.parse(usuariosString);
+    const foundUser = usuarios.find((usuario) => {
+        return usuario.cpf === cpf;
+    });
+    if (foundUser !== undefined) {
+        console.log("CPF já cadastrado");
+        return;
+    }
     usuarios.push({
         nome: nome,
         nascimento: nascimento,
@@ -22,5 +37,8 @@ function criarConta(nome, cpf, nascimento) {
     fs_1.default.writeFileSync(usuariosFilePath, usuariosJson);
 }
 ;
-criarConta('Nathalia', '10/07/1995', '444.555.555-44');
+const nome = process.argv[2];
+const cpf = process.argv[3];
+const nascimento = process.argv[4];
+criarConta(nome, cpf, nascimento);
 //# sourceMappingURL=index.js.map
--- semana-14/projeto/src/index.ts
+++ semana-14/projeto/src/index.ts
@@ -24,20 +24,35 @@ type conta = {
     saldoAtual: number,
     extrato: ExtratoType[]
 };
-// type transacoes = {
 
-// };
+// TO DO, sem cpf iguais
+function criarConta(nome: string, nascimento: string, cpf: string){
+    //VALIANDO A IDADE
+    const dataNasc = moment(nascimento, "DD/MM/YYYY");
+    const hoje = moment();
+    const idade = hoje.diff(dataNasc, "years");
 
-// TO DO, saldo 0 - sem cpf iguais
-function criarConta(nome: string, cpf: string, nascimento: string){
+    if(idade < 18){
+        console.log("Necessário que o usuário tenha mais de 18 anos")
+        return;
+    };
+    
     // PEGAR ARQUIVO DO JSON E CONVERTER EM STRING[]
     const usuariosFilePath = "C:/Users/Larissa/Downloads/Repositório - GitHub/Nathalia-Costa/semana-14/projeto/src/usuarios.json"
     const usuariosFileData: Buffer = fs.readFileSync(usuariosFilePath);
     const usuariosString: string  = usuariosFileData.toString();
-    console.log(usuariosString);
     
-    //CONVERTENDO COM PARSE
+    //CONVERTENDO STRING PARA OBJETO
     const usuarios: conta[] = JSON.parse(usuariosString);
+
+    //VALIDAR CPF
+    const foundUser = usuarios.find((usuario: conta) =>{
+        return usuario.cpf === cpf
+    })
+    if (foundUser !== undefined){
+        console.log("CPF já cadastrado");
+        return
+    }
     // ADICIONANDO CONTA
     usuarios.push({
         nome: nome,
@@ -47,10 +62,15 @@ function criarConta(nome: string, cpf: string, nascimento: string){
         extrato: []
     });
     console.log(usuarios)
-    // CONVERTENDO DE STRING EM JSON
+
+    // CONVERTENDO DE OBJETO PARA STRING
     const usuariosJson = JSON.stringify(usuarios, null, 2);
+
     // ENVIANDO O ARQUIVO CONVERTIDO PARA O FILE JSON
     fs.writeFileSync(usuariosFilePath, usuariosJson)
 };
+const nome:string = process.argv[2];
+const cpf:string = process.argv[3];
+const nascimento:string = process.argv[4];
 
-criarConta('Nathalia','10/07/1995', '444.555.555-44');
+criarConta(nome, cpf, nascimento);
