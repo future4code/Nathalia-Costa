6a53d1024e541534318ca66bb42ef1cdd59bb29a
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-07-01 23:20:20 -0300

    exercicio 08 concluido

---
+++ semana-15/aula49/src/Seller.ts
@@ -0,0 +1,5 @@
+import { Employee } from "./Employee";
+
+export class Seller extends Employee{
+    
+}
--- semana-15/aula49/src/index.ts
+++ semana-15/aula49/src/index.ts
@@ -1,6 +1,7 @@
 import { User } from './User'
 import { Customer } from './Customer'
 import { Employee } from './Employee'
+import { Seller } from './Seller'
 
 const userInstancia: User = new User('1', 'astro@dev.com', 'Astrodev', '123456')
 
@@ -19,12 +20,26 @@ const CustomerInstancia: Customer = new Customer('2', 'nath@dev.com', 'nath', '9
 // console.log("IntrocuceYourself: ", CustomerInstancia.introduceYourself())
 
 const EmployeeInstancia: Employee = new Employee('3', 'lili@dev.com', 'Lica', '741258', "08/05/2018", 1856)
-console.log(EmployeeInstancia)
-console.log('EmployeeID:', EmployeeInstancia.getId())
-console.log('EmployeeName:', EmployeeInstancia.getName())
-console.log('EmployeeEmail:', EmployeeInstancia.getEmail())
-console.log('EmployeeBaseSalary:', EmployeeInstancia.getBaseSalary())
-console.log('EmployeeAdmissionDate:', EmployeeInstancia.getAdmissionDate())
-console.log('EmployeeAdmissionDate:', EmployeeInstancia.calculateTotalSalary())
+// console.log(EmployeeInstancia)
+// console.log('EmployeeID:', EmployeeInstancia.getId())
+// console.log('EmployeeName:', EmployeeInstancia.getName())
+// console.log('EmployeeEmail:', EmployeeInstancia.getEmail())
+// console.log('EmployeeBaseSalary:', EmployeeInstancia.getBaseSalary())
+// console.log('EmployeeAdmissionDate:', EmployeeInstancia.getAdmissionDate())
+// console.log('EmployeeAdmissionDate:', EmployeeInstancia.calculateTotalSalary())
 // 6. a - O construtor da classe user foi chamado User.
-// b. console log em EmployeeInstancia --> id, email, name, password, admissionDate e baseSalary.
+// b - console log --> id, email, name, password, admissionDate e baseSalary.
+
+const SellerInstancia: Seller = new Seller('4', 'kio@dev.com', 'kio', '369852', '05/08/2008', 2500)
+
+console.log(SellerInstancia)
+console.log('SellerID:', SellerInstancia.getId())
+console.log('SellerName:', SellerInstancia.getName())
+console.log('SellerEmail:', SellerInstancia.getEmail())
+console.log('SellerBaseSalary: R$', SellerInstancia.getBaseSalary())
+console.log('SellerAdmissionDate:', SellerInstancia.getAdmissionDate())
+console.log('SellerCalculateTotalSalary: R$', SellerInstancia.calculateTotalSalary())
+// 8. a - id, email, name, password, admissionDate e baseSalary.
+// b - Imprimiu tudo. SellerID, SellerName, SellerEmail, SellerBaseSalary, 
+// SellerAdmissionDate, SellerCalculateTotalSalary
+
