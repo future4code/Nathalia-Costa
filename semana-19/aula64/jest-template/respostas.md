03bea8e2283236414c3bc187e05742c9bb86690f
Nathália Constantino Costa <n.athiccosta@gmail.com>
2020-07-28 15:41:12 -0300

    Exercicio 02

--- semana-19/aula64/jest-template/respostas.md
+++ semana-19/aula64/jest-template/respostas.md
@@ -1,2 +1,5 @@
 # Respostas
 
+#### Exercicio 02
+
+b. Ele espera receber um number por conta da interface ligada aos parametros recebido na função.
--- semana-19/aula64/jest-template/src/Exercicio01.ts
+++ semana-19/aula64/jest-template/src/Exercicio01.ts
@@ -6,7 +6,7 @@ export interface Character{
 };
 
 export function validateCharacter(input: Character): boolean{
-    if (!input.name || input.life === undefined || input.strength || input.defense === undefined){
+    if (!input.name || input.life === undefined || input.strength === undefined || input.defense === undefined){
         return false
     }
     if (input.life < 0 || input.strength < 0 || input.defense < 0){
